12f308cf158e78cfb699722430253067b9bbd4d6
Firtch <firtch@gmail.com>
2021-07-08 21:48:56 +0300

    Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const API_KEY = "222ab113a7edf7cb4122d095e1f5198c";
+
+class App extends React.Component {
+  gettingWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    const data = await api_url.json();
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Form weatherMethod={this.gettingWeather} />
+      </div>
+    );
+  }
+}
+
+const Form = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={props.weatherMethod}>
+      <input type="text" name="city" placeholder="Город" />
+      <button>Получить погоду</button>
+    </form>
   );
 }
 
